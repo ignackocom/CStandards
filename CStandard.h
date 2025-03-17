c10b28723aca7a5c912c1d4460fceee9bb5f810c
ignackocom <peter.potrok@outlook.sk>
2025-03-17 13:24:59 +0100

    Add files via upload

---
+++ CStandard.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+* @file      CStandard.h
+* @author    Peter Potrok
+* @copyright Copyright (c) 1994 - 2025
+*            MIT License (see License.txt file)
+* @brief     C Standard definitions
+* @details
+*
+* @see		 https://en.wikipedia.org/wiki/C_(programming_language)
+			 https://en.cppreference.com/w/c/preprocessor/replace#Predefined_macros
+******************************************************************************/
+
+#ifndef CSTANDARD_H
+#define CSTANDARD_H		197801L
+
+
+/* no standard *//* do not use */
+#define CKR		197801L
+#define C78		CKR
+
+/* official standard ANSI X3.159-1989 *//* ANSI C */
+#define C89		198901L
+
+/* official standard ISO/IEC 9899:1990 *//* ANSI/ISO C */
+#define C90		C89
+
+/* official standard ISO/IEC 9899/AMD1:1995 *//* ANSI/ISO C */
+#define C95		199409L
+
+/* official standard ISO/IEC 9899:1999 *//* ANSI/ISO C */
+#define C99		199901L
+
+/* official standard ISO/IEC 9899:2011 *//* ANSI/ISO C */
+#define C11		201112L
+
+/* official standard ISO/IEC 9899:2018 *//* ANSI/ISO C */
+#define C17		201710L
+
+/* official standard ISO/IEC 9899:2024 *//* ANSI/ISO C */
+#define C23		202311L
+
+
+#endif /* CSTANDARD_H */
